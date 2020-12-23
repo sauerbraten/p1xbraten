f57171b96f3367fe235033844cb5de1d0bf62caf
sauerbraten <willing.alexander@googlemail.com>
2020-12-23 10:07:52 +0100

    initialize extrapings and p1xbratenconfidence

--- src/fpsgame/game.h
+++ src/fpsgame/game.h
@@ -569,7 +569,7 @@ struct fpsent : dynent, fpsstate
 
     vec muzzle;
 
-    fpsent() : weight(100), clientnum(-1), privilege(PRIV_NONE), lastupdate(0), plag(0), ping(0), lifesequence(0), respawned(-1), suicided(-1), lastpain(0), attacksound(-1), attackchan(-1), idlesound(-1), idlechan(-1), frags(0), flags(0), deaths(0), totaldamage(0), totalshots(0), suicides(0), edit(NULL), smoothmillis(-1), playermodel(-1), ai(NULL), ownernum(-1), muzzle(-1, -1, -1)
+    fpsent() : weight(100), clientnum(-1), privilege(PRIV_NONE), lastupdate(0), plag(0), ping(0), lifesequence(0), respawned(-1), suicided(-1), lastpain(0), attacksound(-1), attackchan(-1), idlesound(-1), idlechan(-1), frags(0), flags(0), deaths(0), totaldamage(0), totalshots(0), suicides(0), edit(NULL), smoothmillis(-1), playermodel(-1), ai(NULL), ownernum(-1), extrapings(0), p1xbratenconfidence(0), muzzle(-1, -1, -1)
     {
         name[0] = team[0] = info[0] = 0;
         fragmessages = new vector<fragmessage>;
