c3645fc8c31fff1bcc16140ba3c9fe151e160772
eihrul <lsalzman@gmail.com>
2021-09-29 19:16:45 +0200

    svn r6519

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -3184,7 +3184,7 @@ namespace server
                 {
                     ci->state.editstate = ci->state.state;
                     ci->state.state = CS_EDITING;
-                    ci->events.setsize(0);
+                    ci->events.deletecontents();
                     ci->state.rockets.reset();
                     ci->state.grenades.reset();
                 }
