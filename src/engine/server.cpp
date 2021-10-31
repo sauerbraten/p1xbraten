9bf680c4580ad1cd328ab55b8af22e0e9ba21ef2
sauerbraten <willing.alexander@googlemail.com>
2021-10-31 19:18:54 +0100

    fix walltime rollover in serverlogging.patch

--- src/engine/server.cpp
+++ src/engine/server.cpp
@@ -54,7 +54,7 @@ static void writelog(FILE *file, const char *buf)
     size_t len = strlen(buf), carry = 0;
     if(isdedicatedserver() && logtime)
     {
-        if(!walltime) { walltime = time(NULL); walltime -= totalmillis/1000; if(!walltime) walltime++; }
+        if(!walltime || walltime > INT_MAX/2) { walltime = time(NULL); walltime -= totalmillis/1000; if(!walltime) walltime++; }
         time_t walloffset = walltime + totalmillis/1000;
         struct tm *localvals = localtime(&walloffset);
         static string ts;
