5f4e1d3419af272104e904ba99ed7452abd0eff8
sauerbraten <willing.alexander@googlemail.com>
2023-01-13 17:09:19 +0100

    fix bans.patch on windows
    Vista and newer can use ws2tcpip.h, which has inet_pton

--- src/shared/cube.h
+++ src/shared/cube.h
@@ -22,8 +22,9 @@
   #ifdef _WIN32_WINNT
   #undef _WIN32_WINNT
   #endif
-  #define _WIN32_WINNT 0x0500
+  #define _WIN32_WINNT _WIN32_WINNT_VISTA
   #include "windows.h"
+  #include <ws2tcpip.h>
   #ifndef _WINDOWS
     #define _WINDOWS
   #endif
